e4c9a7293986568db4ff22efa590d07fdebf48e9
Saravanan <saravanan.s@sigmoidanalytics.com>
2025-06-05 22:08:55 +0530

    eval listener

---
+++ eval_listener.py
@@ -0,0 +1,60 @@
+import math
+from eqparser.MathExprListener import MathExprListener
+from eqparser.MathExprParser import MathExprParser
+
+class EvalListener(MathExprListener):
+    def __init__(self, variables=None):
+        self.stack = []
+        self.vars = variables or {}
+
+    def exitNumberExpr(self, ctx:MathExprParser.NumberExprContext):
+        self.stack.append(float(ctx.getText()))
+
+    def exitVariableExpr(self, ctx:MathExprParser.VariableExprContext):
+        var = ctx.getText()
+        self.stack.append(self.vars.get(var, 0))
+
+    def exitNegateExpr(self, ctx:MathExprParser.NegateExprContext):
+        val = self.stack.pop()
+        self.stack.append(-val)
+
+    def exitParensExpr(self, ctx:MathExprParser.ParensExprContext):
+        # No action needed, result already on stack
+        pass
+
+    def exitPowerExpr(self, ctx:MathExprParser.PowerExprContext):
+        right = self.stack.pop()
+        left = self.stack.pop()
+        print(left, right)
+        self.stack.append(left ** right)
+
+    def exitMulDivExpr(self, ctx:MathExprParser.MulDivExprContext):
+        right = self.stack.pop()
+        left = self.stack.pop()
+        if ctx.op.text == '*':
+            self.stack.append(left * right)
+        else:
+            self.stack.append(left / right)
+
+    def exitAddSubExpr(self, ctx:MathExprParser.AddSubExprContext):
+        right = self.stack.pop()
+        left = self.stack.pop()
+        if ctx.op.text == '+':
+            self.stack.append(left + right)
+        else:
+            self.stack.append(left - right)
+
+    def exitFunctionExpr(self, ctx:MathExprParser.FunctionExprContext):
+        arg = self.stack.pop()
+        fname = ctx.function().FUNC_NAME().getText()
+        func_map = {
+            'sin': math.sin,
+            'cos': math.cos,
+            'tan': math.tan,
+            'log': math.log,     # natural log
+            'log10': math.log10,
+            'ln': math.log,
+            'sqrt': math.sqrt,
+            'abs': abs,
+        }
+        self.stack.append(func_map[fname](arg))
--- test_parser.py
+++ test_parser.py
@@ -2,22 +2,18 @@ from eqparser.MathExprLexer import MathExprLexer
 from eqparser.MathExprListener import MathExprListener
 from eqparser.MathExprParser import MathExprParser
 from antlr4 import *
+from eval_listener import EvalListener
 
-
-class EvalListener(MathExprListener):
-    def enterEveryRule(self, ctx:ParserRuleContext):
-        print(ctx.getText())
-        
-    
 def main():
-    lexer = MathExprLexer(InputStream("x+1"))
+    lexer = MathExprLexer(InputStream("(x^10) + 1"))
     stream = CommonTokenStream(lexer)
     parser = MathExprParser(stream)
     r = parser.expr()
     walker = ParseTreeWalker()
-    listener = EvalListener()
+    listener = EvalListener({'x': 10})
     walker.walk(listener, r)
     print(r.toStringTree(recog=parser))
+    print(listener.stack.pop())
     
 if __name__ == "__main__":
     main()
